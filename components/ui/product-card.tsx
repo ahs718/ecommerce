a42a0dd40c0f4c4b0e6d6b547370c73e2d334ad7
Aiden Schulman <aidenhschulman@gmail.com>
2023-08-25 19:51:19 -0400

    added individual category and product pages

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,6 +2,7 @@
 
 import { Expand, ShoppingCart } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
@@ -12,8 +13,17 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`product/${data?.id}`);
+  };
+
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* Images and actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
