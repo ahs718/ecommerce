c1dd8f176473a29c2937bc8b073e195931b9caba
Aiden Schulman <aidenhschulman@gmail.com>
2023-08-26 12:30:07 -0400

    created add to cart functionality

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -7,6 +7,7 @@ import { MouseEventHandler } from "react";
 
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
+import useCart from "@/hooks/use-cart";
 import usePreviewModal from "@/hooks/use-preview-modal";
 import { Product } from "@/types";
 
@@ -15,6 +16,7 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const cart = useCart();
   const previewModal = usePreviewModal();
   const router = useRouter();
 
@@ -26,6 +28,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     event.stopPropagation();
 
     previewModal.onOpen(data);
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
   };
 
   return (
@@ -49,7 +57,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
@@ -58,8 +66,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 
       {/* Description */}
       <div>
-        <p className="font-semibold text-lg">{data.name}</p>
-        <p className="text-sm text-gray-500">{data.category.name}</p>
+        <p className="font-semibold text-lg">{data?.name}</p>
+        <p className="text-sm text-gray-500">{data?.category?.name}</p>
       </div>
 
       {/* Price */}
